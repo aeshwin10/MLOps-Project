4a600044d07c44182149b316d5d397bfb4c6ec94
Ashwin Essan <94974005+aeshwin10@users.noreply.github.com>
2024-09-18 21:36:08 +0530

    MongoDB demo added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Ashwin",
+    author_email="ashwin.essan@gmail.com",
+    packages=find_packages()
+)
